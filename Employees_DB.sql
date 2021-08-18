4e3ba9450c48962b18dcbab72c80e4036ae75632
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-08-18 14:05:23 -0400

    Creating Department + Role Data Table initials

---
+++ Employees_DB.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS employees;
+CREATE DATABASE employees;
+USE employees;
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(60)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(25),
+    salary DECIMAL,
+)
